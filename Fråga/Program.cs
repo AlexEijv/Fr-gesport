49b2f9471df9fb82994ee627b87ed180005c432a
Alexander Eijvergard <alexander.eijvergard@elev.ga.ntig.se>
2023-09-12 11:08:14 +0200

    Har gjort klart fråga 1 och 2

---
+++ Fråga/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Välkommen till min legendariska frågesport");
+
+Console.WriteLine("1, Vilket är det bästa spelserien?");
+Console.WriteLine("a) COD b) Halo c) CS");
+string q1 = Console.ReadLine();
+if (q1 =="b")
+{
+Console.WriteLine("Korrekt");
+}
+else
+{
+    Console.WriteLine("Du är fan mongo");
+}
+
+
+Console.WriteLine("2, Bästa vapnet i CS");
+Console.WriteLine("a) AK47 b) AWP c) M4a1-s");
+string q2 = Console.ReadLine();
+if (q1 =="a")
+{
+    Console.WriteLine("Korrekt");
+}
+else{
+    Console.WriteLine("Herregud, kan man vara sämre?");
+}
+
+Console.WriteLine("3, Bästa kniven i CS:GO");
