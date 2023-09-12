f84a572f1f94d25dd757e8ef227e27b0f50701fc
Alexander Eijvergard <alexander.eijvergard@elev.ga.ntig.se>
2023-09-12 13:40:33 +0200

    Gjorde klart den sista frågan

--- Fråga/Program.cs
+++ Fråga/Program.cs
@@ -3,7 +3,7 @@
 Console.WriteLine("1, Vilket är det bästa spelserien?");
 Console.WriteLine("a) COD b) Halo c) CS");
 string q1 = Console.ReadLine();
-if (q1 =="b")
+if (q1 =="c")
 {
 Console.WriteLine("Korrekt");
 }
@@ -16,12 +16,26 @@ else
 Console.WriteLine("2, Bästa vapnet i CS");
 Console.WriteLine("a) AK47 b) AWP c) M4a1-s");
 string q2 = Console.ReadLine();
-if (q1 =="a")
+if (q2 =="a")
 {
     Console.WriteLine("Korrekt");
 }
-else{
+else
+{
     Console.WriteLine("Herregud, kan man vara sämre?");
 }
 
-Console.WriteLine("3, Bästa kniven i CS:GO");
+Console.WriteLine("3, Bästa mid tier kniven i CS:GO");
+Console.WriteLine("a) Nomad b) Skeleton c) Talon");
+string q3 = Console.ReadLine();
+if (q3 == "b")
+{
+    Console.WriteLine("Korrekt");
+}
+else
+{
+    Console.WriteLine("Snälla, lär dig nått");
+}
+
+Console.WriteLine("Tryck på en tangent för att avsluta.");
+Console.ReadKey();
