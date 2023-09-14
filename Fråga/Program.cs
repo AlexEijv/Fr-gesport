d16d0b02d42d7b04b1d43a67e400e1d623d821e2
Alexander Eijvergard <alexander.eijvergard@elev.ga.ntig.se>
2023-09-14 10:28:24 +0200

    Lade till ToLower och ett poäng system

--- Fråga/Program.cs
+++ Fråga/Program.cs
@@ -1,24 +1,30 @@
-﻿Console.WriteLine("Välkommen till min legendariska frågesport");
+﻿Console.ForegroundColor = ConsoleColor.Blue;
+
+int points = 0;
+Console.WriteLine("Välkommen till min legendariska frågesport");
 
 Console.WriteLine("1, Vilket är det bästa spelserien?");
 Console.WriteLine("a) COD b) Halo c) CS");
-string q1 = Console.ReadLine();
+string q1 = Console.ReadLine().ToLower();
+
 if (q1 =="c")
 {
 Console.WriteLine("Korrekt");
+points++;
 }
 else
 {
-    Console.WriteLine("Du är fan mongo");
+    Console.WriteLine("Du har tappat det");
 }
 
 
 Console.WriteLine("2, Vidrigaste vapnet i CS");
 Console.WriteLine("a) P90 b) AWP c) m249");
-string q2 = Console.ReadLine();
+string q2 = Console.ReadLine().ToLower();
 if (q2 =="b")
 {
     Console.WriteLine("Korrekt");
+    points++;
 }
 else
 {
@@ -27,15 +33,17 @@ else
 
 Console.WriteLine("3, Bästa mid tier kniven i CS:GO");
 Console.WriteLine("a) Skeleton b) Nomad c) Talon");
-string q3 = Console.ReadLine();
+string q3 = Console.ReadLine().ToLower();
 if (q3 == "a")
 {
     Console.WriteLine("Korrekt");
+    points++;
 }
 else
 {
     Console.WriteLine("Snälla... Jag blir fan ledsen");
 }
+Console.WriteLine("Det här är dina poäng: " + points);
 
 Console.WriteLine("Tryck på en tangent för att avsluta.");
 Console.ReadKey();
